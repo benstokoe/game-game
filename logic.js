5704b0b415c30a71814f88c62d5bbd24c56358ae
Ben Stokoe <skyhaikuteam+benstokoe@gmail.com>
2015-04-24 10:58:07 +0100

    Pictures get removed from image array for end game

--- logic.js
+++ logic.js
@@ -1,9 +1,18 @@
+var images = $('img');
 var currentPerson = "";
 
 $('#start-game').on('click', function(e) {
     e.preventDefault();
 
+    $(this).hide();
+
     showRandomPicture();
+});
+
+$('#restart-game').on('click', function(e) {
+    e.preventDefault();
+
+    location.reload();
 });
 
 $('#answer-buttons').on('click', function(e) {
@@ -21,22 +30,28 @@ $('#answer-buttons').on('click', function(e) {
     }
 
     setTimeout(function() {
-        showRandomPicture();
+        if (images.length !== 0) {
+            showRandomPicture();
+        } else {
+            showEndScreen();
+        }
     }, 500);
+
 });
 
 var showRandomPicture = function() {
     hideAnswerButtons();
     $('#current-answer').html('');
 
-    var images = $('img');
-    var randomImage = images[randomNumber(images.length)];
+    var imageNumber = randomNumber(images.length);
+    var randomImage = images[imageNumber];
 
     $(randomImage).show();
     currentPerson = randomImage.dataset.person;
 
     setTimeout(function() {
         $(randomImage).hide();
+        images.splice(imageNumber, 1);
         showAnswerButtons();
     }, 300);
 };
@@ -46,6 +61,12 @@ var hideAnswerButtons = function() {
 };
 var showAnswerButtons = function() {
     $('#answer-buttons').show();
+};
+
+var showEndScreen = function() {
+    $('#current-answer').html('');
+    $('#final-text').html('Final ');
+    $('#restart-game').show();
 };
 
 var randomNumber = function(max) {
