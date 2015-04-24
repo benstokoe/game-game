6a1573933115e15bff9de6723f2603de096aa805
Ben Stokoe <skyhaikuteam+benstokoe@gmail.com>
2015-04-24 10:17:10 +0100

    Hide image after certain time and then show answer buttons

--- logic.js
+++ logic.js
@@ -1,15 +1,41 @@
+var currentPerson = "";
+
 $('#start-game').on('click', function(e) {
     e.preventDefault();
 
     showRandomPicture();
 });
 
-var showRandomPicture = function() {
-    var images = $('img');
+$('#answer-buttons').on('click', function(e) {
+    e.preventDefault();
 
+    if (e.target.id === currentPerson) {
+        console.log('CORRECT! ' + currentPerson);
+    }
+
+    showRandomPicture();
+});
+
+var showRandomPicture = function() {
+    hideAnswerButtons();
+
+    var images = $('img');
     var randomImage = images[randomNumber(images.length)];
 
     $(randomImage).show();
+    currentPerson = randomImage.dataset.person;
+
+    setTimeout(function() {
+        $(randomImage).hide();
+        showAnswerButtons();
+    }, 300);
+};
+
+var hideAnswerButtons = function() {
+    $('#answer-buttons').hide();
+};
+var showAnswerButtons = function() {
+    $('#answer-buttons').show();
 };
 
 var randomNumber = function(max) {
